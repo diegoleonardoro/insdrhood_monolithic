d2c99e254840834a377d8e63a20526376d9b9975
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2023-11-01 17:27:28 -0400

    form questions first 3

--- client/src/components/Forms/1/index.js
+++ client/src/components/Forms/1/index.js
@@ -93,8 +93,8 @@ const FormComponent = () => {
   const foodDiversityExplanationRef = useRef(null);
   const neighborhoodInput = useRef();
 
-
-  const onlyNYCResidentsSign = liveinNYCSign === "no" ? "block" : "none";// 
+  const showFormToResident = liveinNYC === "yes" ? "visible" : "hidden";
+  const onlyNYCResidentsSign = liveinNYCSign === "no" ? "block" : "none";
 
 
   // a request to check the currently logged in user needs to be made:
@@ -199,6 +199,11 @@ const FormComponent = () => {
             letsTalkAboutGeneralInfo.current.style.display = "none";
           }, 1000);
         };
+
+        // This if statement sets the value the state "neighborhood", when the "next" arrow is clicked on the neighborhood input.
+        if (currentDiv.className.indexOf("neighborhoodInput") > -1) {
+          setNeighborhood(currentDiv.children[1].value);
+        }
 
 
         // The following if statement will check if the user has clicked the last question
@@ -424,7 +429,7 @@ const FormComponent = () => {
 
 
   return (
-  <div className="mainContainer">
+    <div className="mainContainer">
 
 
 
@@ -527,12 +532,91 @@ const FormComponent = () => {
         </div>
 
 
+        {/** How long have you been living in you neighborhood? */}
+        <div
+          className={
+            "neighborhood nhoodAdjectives lengthLivingInHood " +
+            displayQuestion("yearsInNeighborhood") +
+            " " +
+            shakie
+          }
+          ref={ref => divRefs.current[2] = ref}
+        >
+          <label>
+            How long have you been living in{" "}
+            <span className= "questionHighlight nhoodName">{neighborhood}</span>
+          </label>
+          <select
+            name="time_linving_in_nhood"
+            id="yearsInNeighborhood"
+            className="selectOptions inputCheck"
+            onChange={(e) => {
+              setFormData({
+                ...formData,
+                timeLivingInNeighborhood: e.target.value,
+              });
+            }}
+          >
+            <option value="">Choose an option</option>
+            <option value="Less than one year">Less than one year</option>
+            <option value="Between 1 and 5 years">
+              Between 1 and 5 years
+            </option>
+            <option value="Between 6 and 10 years">
+              Between 6 and 10 years
+            </option>
+            <option value="Between 11 and 15 years">
+              Between 11 and 15 years
+            </option>
+            <option value="Between 16 and 20 years">
+              Between 16 and 20 years
+            </option>
+            <option value="For more than 20 years">
+              For more than 20 years
+            </option>
+            <option value="I do not live in this neighborhood">
+              I do not live in this neighborhood
+            </option>
+          </select>
+        </div>
+
+
+
+
+
+        {/** Arrows */}
+        <div
+          className="arrowsContainer"
+          style={{ visibility: showFormToResident }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            viewBox="0 0 16 16"
+            onClick={() => changeQuestion("prev")}
+          >
+            <path d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+          </svg>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            viewBox="0 0 16 16"
+            onClick={() => changeQuestion("next")}
+          >
+            <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+          </svg>
+        </div>
+
+
       </form>
 
 
 
 
-  </div>
+    </div>
   )
 
 
