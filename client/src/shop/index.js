da475f112b3105f58ac8a9746c9a6f0163821589
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2024-03-14 22:00:03 -0400

    new frame

--- client/src/shop/index.js
+++ client/src/shop/index.js
@@ -14,15 +14,28 @@ const SHOP_DATA = [
   {
     id: 1.2,
     title: 'The Essex Hotel Framed Illustration. Frame8 in. x 10 in. matted to 5 in. x 7 in. Shipping included.',
-    description: " TEXT: 'The JW Marriott Essex House New York, a prestigious hotel towering over New York City, was constructed during the devastating economic downturn following the Wall Street Crash of 1929, its development was significantly slowed, only opening its doors in late 1931. Despite the financial hardships that led to the bankruptcy of its original owners, the Essex House has become a prominent figure in New York City's skyline, thanks in part to its iconic red neon sign added a year after opening.The building was designed by the architectural firm Walker & Gillette, and it showcases an Art Deco architectural style, characteristic of many prominent structures built in New York City during the late 1920s and early 1930s.' " ,
+    description: " TEXT: 'The JW Marriott Essex House New York, a prestigious hotel towering over New York City, was constructed during the devastating economic downturn following the Wall Street Crash of 1929, its development was significantly slowed, only opening its doors in late 1931. Despite the financial hardships that led to the bankruptcy of its original owners, the Essex House has become a prominent figure in New York City's skyline, thanks in part to its iconic red neon sign added a year after opening.The building was designed by the architectural firm Walker & Gillette, and it showcases an Art Deco architectural style, characteristic of many prominent structures built in New York City during the late 1920s and early 1930s.' ",
     imageUrl: [
       'https://raw.githubusercontent.com/diegoleonardoro/multi-k8s/main/essex-frame.jpg',
       'https://raw.githubusercontent.com/diegoleonardoro/multi-k8s/main/essex-frame3.jpg',
       'https://raw.githubusercontent.com/diegoleonardoro/multi-k8s/main/essex-house2.jpg'
     ],
-    price: 26,
+    price: 29,
   },
- 
+
+  {
+    id: 1.3,
+    title: 'The New Yorker Hotel Framed Illustration. Frame8 in. x 10 in. matted to 5 in. x 7 in. Shipping included.',
+    description: " TEXT: The New Yorker Hotel, located at 481 Eighth Avenue in the Hell's Kitchen neighborhood of Manhattan, was designed by architecture firm Sugarman and Berger and opened its doors on January 2, 1930. It's recognized for its Art Deco architectural style, which was prevalent in New York during the 1920s and 1930s, similar to its contemporaries like the Empire State Building and the Chrysler Building. The hotel's facade features alternating vertical bands of warm gray brick and windows, creating an impression of boldly modeled masses, further emphasized by dramatic lighting at night and deep-cut light courts. The Hotel is known for its iconic neon sign.This sign, located on the western facade of the hotel, spells out 'New Yorker' in 21 - foot - high(6.4 m) capital letters.The original sign, which illuminated the skyline from 1941 to 1967, was a prominent feature of the New York City skyline.However, it remained dark for several decades until 2005, when it was replaced with a new LED sign. ",
+    imageUrl: [
+      'https://raw.githubusercontent.com/diegoleonardoro/multi-k8s/main/thenyorker.jpg',
+      'https://raw.githubusercontent.com/diegoleonardoro/multi-k8s/main/newyorker4.jpg',
+      'https://raw.githubusercontent.com/diegoleonardoro/multi-k8s/main/thenewyorker3.jpg',
+     
+    ],
+    price: 29,
+  },
+
 
   {
     id: 2,
@@ -35,11 +48,11 @@ const SHOP_DATA = [
     ],
     price: 20,
   },
-  
 
 
- 
- 
+
+
+
 
 
 
