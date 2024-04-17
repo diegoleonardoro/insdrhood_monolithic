2a500797cc0334637997adcdf215d0c2168fc682
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2024-04-17 16:54:01 -0400

    newsletter fileting

--- backend/dist/database/repositories/newsletter.js
+++ backend/dist/database/repositories/newsletter.js
@@ -8,10 +8,60 @@ const index_1 = require("../index");
 const bad_request_error_1 = require("../../errors/bad-request-error");
 const emailVerification_1 = require("../../services/emailVerification");
 const mail_1 = __importDefault(require("@sendgrid/mail"));
+const mongodb_1 = require("mongodb");
 class NewsletterRepository {
     constructor() {
         this.collectionName = 'newsletter';
         this.db = (0, index_1.connectToDatabase)();
+    }
+    ;
+    async fetchSubscribers(frequency) {
+        const db = await this.db;
+        const emailsCollection = db.collection(this.collectionName);
+        const now = Date.now();
+        if (frequency === 'allofthem') {
+            return emailsCollection.find({}, { projection: { email: 1, name: 1, frequency: 1, _id: 0 } } // also return the unique identifier 
+            ).toArray();
+        }
+        else {
+            const desiredFrequencyInWeeks = parseInt(frequency);
+            const desiredFrequencyInMs = desiredFrequencyInWeeks * 7 * 24 * 60 * 60 * 1000;
+            return emailsCollection.find({
+                frequency: frequency,
+                // Check if lastSent is either missing or greater than or equal to desired frequency
+                $or: [
+                    { lastSent: { $exists: false } },
+                    { $expr: { $gte: [{ $subtract: [now, '$lastSent'] }, desiredFrequencyInMs] } }
+                ]
+            }, { projection: { email: 1, name: 1, frequency: 1, _id: 0 } }).toArray();
+        }
+        ;
+    }
+    ;
+    async sendEmails(subscribers) {
+        if (subscribers.length === 0) {
+            return { message: "No subscribers.", statusCode: 404 };
+        }
+        const msg = {
+            from: { name: "Insider Hood", email: "admin@insiderhood.com" },
+            personalizations: subscribers.map(subscriber => ({
+                to: [{ email: subscriber.email }],
+                dynamic_template_data: { name: subscriber.name }
+                // add unique identifier for user which will be used to give the user the option to unsubscribe or change the frequency they receive emails
+            })),
+            templateId: "d-007c132db5d3481996d2e3720cb9fdce"
+        };
+        await mail_1.default.send(msg);
+        const ids = subscribers.map(sub => sub._id);
+        await this.updateLastSent(ids);
+        return { message: "Newsletter Sent", statusCode: 200 };
+    }
+    async updateLastSent(ids) {
+        const db = await this.db;
+        const emailsCollection = db.collection(this.collectionName);
+        // Convert string array to ObjectId array
+        const objectIds = ids.map(id => new mongodb_1.ObjectId(id));
+        await emailsCollection.updateMany({ _id: { $in: objectIds } }, { $set: { lastSent: new Date() } });
     }
     async subscribeToNewsletter(data) {
         const db = await this.db;
@@ -30,38 +80,15 @@ class NewsletterRepository {
         });
         return { message: "Email subscribed" };
     }
+    ;
     async sendNewsLetter(data) {
-        mail_1.default.setApiKey(process.env.SENDGRID_API_KEY);
-        const db = await this.db;
-        const { frequency } = data;
-        const emailsCollection = db.collection(this.collectionName);
         try {
-            const subscribersDocuments = await emailsCollection.find({ frequency: frequency }, { projection: { email: 1, _id: 0 } }).toArray();
-            // Extract the email addresses from the documents
-            const subscribers = subscribersDocuments.map(doc => doc.email);
-            if (subscribers.length === 0) {
-                return { message: `No subscribers.`, statusCode: 500 };
-            }
-            else {
-                const msg = {
-                    to: subscribers,
-                    from: {
-                        name: "Insider Hood",
-                        email: "admin@insiderhood.com"
-                    },
-                    // dynamic_template_data: {
-                    //   name: "Diego"
-                    // },
-                    templateId: "d-007c132db5d3481996d2e3720cb9fdce"
-                };
-                await mail_1.default.send(msg);
-                return { message: "Newsletter Sent", statusCode: 200 };
-            }
-            ;
+            const subscribers = await this.fetchSubscribers(data.frequency);
+            return await this.sendEmails(subscribers);
         }
         catch (error) {
-            console.error("Error retrieving subscribers:", error); // Catch and log any errors during the query
-            return { message: `Failed to send newsletter.`, statusCode: 500 };
+            console.error("Error processing newsletter:", error);
+            return { message: "Failed to send newsletter.", statusCode: 500 };
         }
     }
     ;
--- backend/src/database/repositories/newsletter.ts
+++ backend/src/database/repositories/newsletter.ts
@@ -3,14 +3,76 @@ import { connectToDatabase } from '../index';
 import { BadRequestError } from '../../errors/bad-request-error';
 import { sendNewsLetterEmail } from '../../services/emailVerification';
 import sgMail from '@sendgrid/mail';
+import { ObjectId } from 'mongodb';
 
 
 export class NewsletterRepository {
+
   private db: Promise<Db>;
   private collectionName = 'newsletter';
 
   constructor() {
     this.db = connectToDatabase();
+  };
+
+
+  private async fetchSubscribers(frequency: string) {
+    const db = await this.db;
+    const emailsCollection = db.collection(this.collectionName);
+    const now = Date.now();
+
+    if (frequency === 'allofthem') {
+      return emailsCollection.find(
+        {},
+        { projection: { email: 1, name: 1, frequency: 1, _id: 0 } } // also return the unique identifier 
+      ).toArray();
+    } else {
+
+      const desiredFrequencyInWeeks = parseInt(frequency);
+      const desiredFrequencyInMs = desiredFrequencyInWeeks * 7 * 24 * 60 * 60 * 1000;
+      return emailsCollection.find({
+        frequency: frequency,
+        // Check if lastSent is either missing or greater than or equal to desired frequency
+        $or: [
+          { lastSent: { $exists: false } },
+          { $expr: { $gte: [{ $subtract: [now, '$lastSent'] }, desiredFrequencyInMs] } } 
+        ]
+      }, { projection: { email: 1, name: 1, frequency: 1, _id: 0 } }).toArray();
+    };
+  };
+
+
+  private async sendEmails(subscribers: any[]) {
+    if (subscribers.length === 0) {
+      return { message: "No subscribers.", statusCode: 404 };
+    }
+
+    const msg = {
+      from: { name: "Insider Hood", email: "admin@insiderhood.com" },
+      personalizations: subscribers.map(subscriber => ({
+        to: [{ email: subscriber.email }],
+        dynamic_template_data: { name: subscriber.name }
+        // add unique identifier for user which will be used to give the user the option to unsubscribe or change the frequency they receive emails
+      })),
+      templateId: "d-007c132db5d3481996d2e3720cb9fdce"
+    };
+
+    await sgMail.send(msg);
+    const ids = subscribers.map(sub => sub._id);
+    await this.updateLastSent(ids);
+    return { message: "Newsletter Sent", statusCode: 200 };
+
+  }
+
+  private async updateLastSent(ids: string[]) {
+    const db = await this.db;
+    const emailsCollection = db.collection(this.collectionName);
+    // Convert string array to ObjectId array
+    const objectIds = ids.map(id => new ObjectId(id));
+    await emailsCollection.updateMany(
+      { _id: { $in: objectIds } },
+      { $set: { lastSent: new Date() } }
+    );
   }
 
   async subscribeToNewsletter(data: { email: string, name?: string, newsletter: boolean, frequency?: string }): Promise<{ message: string }> {
@@ -33,50 +95,23 @@ export class NewsletterRepository {
     });
     return { message: "Email subscribed" };
 
-  }
+  };
 
+  async sendNewsLetter(data: { frequency: string }): Promise<{ message: string, statusCode: number }> {
 
-  async sendNewsLetter(data: { frequency: string, }): Promise<{ message: string, statusCode: number }> {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
-    const db = await this.db;
-    const { frequency } = data;
-    const emailsCollection = db.collection(this.collectionName);
     try {
-      const subscribersDocuments = await emailsCollection.find(
-        { frequency: frequency },
-        { projection: { email: 1, _id: 0 } }
-      ).toArray();
-
-      // Extract the email addresses from the documents
-      const subscribers = subscribersDocuments.map(doc => doc.email);
-
-      if (subscribers.length === 0) {
-        return { message: `No subscribers.`, statusCode: 500 }
-      } else {
-
-        const msg = {
-          to: subscribers, // Ensure this is an array of email strings
-          from: {
-            name: "Insider Hood",
-            email: "admin@insiderhood.com"
-          },
-          // dynamic_template_data: {
-          //   name: "Diego"
-          // },
-          templateId: "d-007c132db5d3481996d2e3720cb9fdce"
-        }
-        await sgMail.send(msg);
-        return { message: "Newsletter Sent", statusCode: 200 };
-      };
-
+      const subscribers = await this.fetchSubscribers(data.frequency);      
+      return await this.sendEmails(subscribers);
     } catch (error) {
-      console.error("Error retrieving subscribers:", error); // Catch and log any errors during the query
-      return { message: `Failed to send newsletter.`, statusCode: 500 }
+      console.error("Error processing newsletter:", error);
+      return { message: "Failed to send newsletter.", statusCode: 500 };
     }
+  
   };
 
 
-  async sendReferralEmail(data: { email: string, templateId: string }): Promise<{ message: string, statusCode:number }> {
+
+  async sendReferralEmail(data: { email: string, templateId: string }): Promise<{ message: string, statusCode: number }> {
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
     const { email, templateId } = data;
@@ -97,15 +132,14 @@ export class NewsletterRepository {
         templateId: templateId
       };
       await sgMail.send(msg);
-      return { message: "Newsletter Sent", statusCode:200 };
+      return { message: "Newsletter Sent", statusCode: 200 };
 
     } catch (error) {
       console.error("Error retrieving subscribers:", error); // Catch and log any errors during the query
-      return { message: `Failed to send newsletter.`, statusCode: 500}
+      return { message: `Failed to send newsletter.`, statusCode: 500 }
     }
 
   }
-
 
 
 
