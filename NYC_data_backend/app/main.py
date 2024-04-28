215404e14be8eacf60d79ab3564c189d3a136ac4
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2024-04-28 18:31:30 -0400

    new python img

--- NYC_data_backend/app/main.py
+++ NYC_data_backend/app/main.py
@@ -1,1 +1,13 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google Cloud Run,
+    # a webserver process such as Gunicorn will serve the app.
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
