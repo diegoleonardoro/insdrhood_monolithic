861b9888810583864ac8dac3943f8bee94d40dc9
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2024-05-06 12:00:21 -0400

    cors

--- NYC_data_backend/app/main.py
+++ NYC_data_backend/app/main.py
@@ -23,7 +23,7 @@ def hello_world():
     return 'Hello, Worls!'
 
 @app.route('/311calls', methods=['GET'])
-@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])  # Ensure CORS settings are as intended
+@cross_origin(origin='*', supports_credentials=True)
 def calls311():
     # API URL with a limit to avoid fetching too much data at once (you might need to paginate or adjust this)
     response = requests.get('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
