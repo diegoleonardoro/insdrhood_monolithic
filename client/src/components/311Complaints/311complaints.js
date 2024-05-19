8efcb03cad440a15a1dcb0dcd971cea78df08eac
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2024-05-19 13:25:07 -0400

    loading

--- client/src/components/311Complaints/311complaints.js
+++ client/src/components/311Complaints/311complaints.js
@@ -14,7 +14,7 @@ const Complaints311 = () => {
 
   const [complaints, setComplaints] = useState([]);
   const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [hasMore, setHasMore] = useState(true);
 
   const [filters, setFilters] = useState({
@@ -48,8 +48,6 @@ const Complaints311 = () => {
     } catch (error) {
 
     }
-
-
 
   };
 
@@ -95,6 +93,8 @@ const Complaints311 = () => {
   useEffect(() => {
     if (initialLoad) {
       fetchComplaints(true, false);
+
+
     }
   }, [initialLoad]);
 
@@ -163,6 +163,7 @@ const Complaints311 = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: "100vh" }}>
 
+      {/* Always render the form */}
       <Form onSubmit={handleFilterSubmit} style={{ width: '85%', maxWidth: '400px', margin: '40px' }}>
         <Form.Group controlId="formZip">
           <Form.Label>Incident Zip:</Form.Label>
@@ -192,96 +193,73 @@ const Complaints311 = () => {
             <option value="STATEN ISLAND">Staten Island</option>
           </Form.Control>
         </Form.Group>
-        {/* <Form.Group controlId="formAgency">
-            <Form.Label>Agency</Form.Label>
-            <Form.Control
-              as="select"
-              name="Agency"
-              value={filters.Agency}
-              onChange={handleFilterChange}
-            >
-              <option value="">Show All</option>
-              <option value="NYPD">NYPD - New York Police Department</option>
-              <option value="DOT">DOT - Department of Transportation</option>
-              <option value="DHS">DHS - Department of Homeless Services</option>
-              <option value="DOHMH">DOHMH - Department of Health and Mental Hygiene</option>
-              <option value="HPD">HPD - Housing Preservation and Development</option>
-              <option value="DPR">DPR - Department of Parks and Recreation</option>
-              <option value="DSNY">DSNY - Department of Sanitation</option>
-              <option value="DCWP">DCWP - Department of Consumer and Worker Protection</option>
-              <option value="TLC">TLC - Taxi and Limousine Commission</option>
-              <option value="EDC">EDC - Economic Development Corporation</option>
-              <option value="DEP">DEP - Department of Environmental Protection</option>
-            </Form.Control>
-          </Form.Group> */}
-        {/* <Form.Group controlId="formDate">
-          <Form.Label>Created Date</Form.Label>
-          <Form.Control
-            type="date"
-            name="CreatedDate"
-            value={filters.CreatedDate}
-            onChange={handleFilterChange}
-          />
-        </Form.Group> */}
         <Button className="filter311button" style={{ marginTop: "20px", width: "100%", backgroundColor: "rgba(255, 151, 5, 0.221)", color: "black" }} type="submit" variant="dark">Apply Filters</Button>
       </Form>
 
-      <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', backgroundColor: "#F4E1D2" }}>
-        {complaints.map((complaint, index) => (
-          <Card className="Cards311" style={{ width: '18rem', margin: "20px" }} key={index}>
-            <ListGroup className="list-group-flush Cards_Group">
-              <Card.Header className="Cards_Group_1" as="h5">{titleCase(complaint['Borough'])}, {complaint['Incident Zip']}</Card.Header>
-              <ListGroup.Item className="Cards_Group_2"> {complaint['Descriptor']}. {complaint['Complaint Type']} </ListGroup.Item>
-              <ListGroup.Item className="Cards_Group_3" > <span style={{ fontWeight: "bold" }} >Address: </span>{titleCase(complaint['Incident Address'])}</ListGroup.Item>
-              <ListGroup.Item className="Cards_Group_4" ><span style={{ fontWeight: "bold" }}>Issued: </span>{formatDate(complaint['Created Date'])}</ListGroup.Item>
-              <ListGroup.Item className="Cards_Group_5"><span style={{ fontWeight: "bold" }} >Responding agency: </span>{complaint['Agency']}</ListGroup.Item>
-            </ListGroup>
-          </Card>
-        ))}
-        {loading && <div>
-          <Spinner animation="grow" size="sm" className="spinner spinner1" />
-          <Spinner animation="grow" className="spinner spinner2" />
-          <Spinner animation="grow" style={{ height: "50px", width: "50px" }} className="spinner spinner3" />
-        </div>}
-        {!hasMore && <div>No more complaints to show.</div>}
-        {hasMore && !loading && <Button style={{ padding: "15px", alignSelf: "center" }} variant="dark" onClick={() => fetchComplaints()}>Load More</Button>}
-
-      </div>
-
-
-      {/* Sticky signup form at the bottom */}
-
-      {showNewsletterForm && (
-        <div className={`newsletter-form ${formVisible ? 'expanded' : 'collapsed'}`} style={{ transition: 'height 0.3s ease-in-out', padding: "2px" }}>
-          <div style={{padding:"15px", paddingBottom:"30px"}}>
-            <p className='p_signup311Complaints'>Register for 311 Updates in your Zipcode:</p>
-            <Form className="signup311Complaints" onSubmit={handleNewsletterSubmit} style={{ display: 'flex', alignItems: 'center' }}>
-              {formVisible ? (
-                <>
-
-                  <Form.Group className="emailcomplaintsnewsletter" style={{ width: '90%' }}>
-                    <Form.Control className='input311Form' type="email" name="email" placeholder="Enter email" value={newsletter.email} onChange={handleNewsletterChange} required />
-                  </Form.Group>
-                  <Form.Group style={{ width: '90%' }}>
-                    <Form.Control className='input311Form' type="text" name="zipCode" placeholder="Zip Code" value={newsletter.zipCode} onChange={handleNewsletterChange} required />
-                  </Form.Group>
-                  <div style={{ display: 'flex', justifyContent: 'flex-start', width: '100%', paddingLeft: '10px' }}>
-                    <Button style={{ width: "65%", height: "30px", backgroundColor: "#ffc107", color: "black", borderColor: "black", fontSize: "12px" }} type="submit" variant="primary">Register</Button>
-                    <Button style={{ width: "35%", height: "30px", alignSelf: "center", fontSize: "10px" }} variant="outline-secondary" onClick={toggleForm}>Close</Button>
-                  </div>
-                </>
-              ) : (
-                <Button className="show311Form"variant="outline-secondary" onClick={toggleForm} style={{ width: '100%', marginBottom:"10px", height:"25px" }}>Sign Up for Updates</Button>
-              )}
-            </Form>
+      {/* Conditional rendering based on loading for the remaining content */}
+      {!loading && (
+        <>
+          <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', backgroundColor: "#F4E1D2" }}>
+            {complaints.map((complaint, index) => (
+              <Card className="Cards311" style={{ width: '18rem', margin: "20px" }} key={index}>
+                <ListGroup className="list-group-flush Cards_Group">
+                  <Card.Header className="Cards_Group_1" as="h5">{titleCase(complaint['Borough'])}, {complaint['Incident Zip']}</Card.Header>
+                  <ListGroup.Item className="Cards_Group_2"> {complaint['Descriptor']}. {complaint['Complaint Type']} </ListGroup.Item>
+                  <ListGroup.Item className="Cards_Group_3" > <span style={{ fontWeight: "bold" }} >Address: </span>{titleCase(complaint['Incident Address'])}</ListGroup.Item>
+                  <ListGroup.Item className="Cards_Group_4" ><span style={{ fontWeight: "bold" }}>Issued: </span>{formatDate(complaint['Created Date'])}</ListGroup.Item>
+                  <ListGroup.Item className="Cards_Group_5"><span style={{ fontWeight: "bold" }} >Responding agency: </span>{complaint['Agency']}</ListGroup.Item>
+                </ListGroup>
+              </Card>
+            ))}
+            {!hasMore && <div>No more complaints to show.</div>}
+            {hasMore && <Button style={{ padding: "15px", alignSelf: "center" }} variant="dark" onClick={() => fetchComplaints()}>Load More</Button>}
           </div>
-        </div>
 
+          {showNewsletterForm && (
+            <div className={`newsletter-form ${formVisible ? 'expanded' : 'collapsed'}`} style={{ transition: 'height 0.3s ease-in-out', padding: "2px" }}>
+              <div style={{ padding: "15px", paddingBottom: "30px" }}>
+                <p className='p_signup311Complaints'>Register for 311 Updates in your Zipcode:</p>
+                <Form className="signup311Complaints" onSubmit={handleNewsletterSubmit} style={{ display: 'flex', alignItems: 'center' }}>
+                  {formVisible ? (
+                    <>
+                      <Form.Group className="emailcomplaintsnewsletter" style={{ width: '90%' }}>
+                        <Form.Control className='input311Form' type="email" name="email" placeholder="Enter email" value={newsletter.email} onChange={handleNewsletterChange} required />
+                      </Form.Group>
+                      <Form.Group style={{ width: '90%' }}>
+                        <Form.Control className='input311Form' type="text" name="zipCode" placeholder="Zip Code" value={newsletter.zipCode} onChange={handleNewsletterChange} required />
+                      </Form.Group>
+                      <div style={{ display: 'flex', justifyContent: 'flex-start', width: '100%', paddingLeft: '10px' }}>
+                        <Button style={{ width: "65%", height: "30px", backgroundColor: "#ffc107", color: "black", borderColor: "black", fontSize: "12px" }} type="submit" variant="primary">Register</Button>
+                        <Button style={{ width: "35%", height: "30px", alignSelf: "center", fontSize: "10px" }} variant="outline-secondary" onClick={toggleForm}>Close</Button>
+                      </div>
+                    </>
+                  ) : (
+                    <Button className="show311Form" variant="outline-secondary" onClick={toggleForm} style={{ width: '100%', marginBottom: "10px", height: "25px" }}>Sign Up for Updates</Button>
+                  )}
+                </Form>
+              </div>
+            </div>
+          )}
+        </>
       )}
 
-
+      {/* Loading spinner that only appears when the content is being loaded */}
+      {loading && (
+        <div style={{ height: "87.5vh" }}>
+          <div style={{ position: "relative", left: "45%", top: "30%" }}>
+            <Spinner animation="grow" size="sm" className="spinner spinner1" />
+            <Spinner animation="grow" className="spinner spinner2" />
+            <Spinner animation="grow" style={{ height: "50px", width: "50px" }} className="spinner spinner3" />
+          </div>
+        </div>
+      )}
     </div>
   );
+
+
+
+
+
 
 
 
